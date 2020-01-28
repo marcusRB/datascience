95722a3e2ab1e19f709d0608204c48cc0cecf235
rubrimedia <rubrimedia@gmail.com>
2020-01-28 14:30:32 +0100

    update

---
+++ DSA2019/R_introduction.R
@@ -0,0 +1,29 @@
+# Calculate 3 + 4
+3 + 4
+
+# Calculate 6 + 12
+
+
+# An addition
+5 + 5 
+
+# A subtraction
+5 - 5 
+
+# A multiplication
+3 * 5
+
+# A division
+(5 + 5) / 2 
+
+# Exponentiation
+
+
+# Modulo
+
+
+# Assign the value 42 to x
+x <- 
+  
+# Print out the value of the variable x
+x
